ef40c28050ddda953f3d6ca293547db9c2f2ec9a
Yana <qwq.yana@yandex.ru>
2024-09-02 13:09:30 +0300

    верстка с каруселькой на js

---
+++ promo Indonesia/script.js
@@ -0,0 +1,54 @@
+// каруселька
+
+const image_options = [
+    "/img/image2.jpg",
+    "/img/bg3.jpg",
+    "/img/bg4.jpg",
+    "/img/bg5.jpg"
+];
+
+let i = 0;
+
+const currentOptionImage = document.querySelector(".background");
+const carousel = document.querySelector(".carousel-wrapper-block");
+const optionPrevious = document.querySelector(".previous-option");
+const optionNext = document.querySelector(".next-option");
+
+
+currentOptionImage.style.backgroundImage = "url(" + image_options[i] + ")";
+
+optionNext.onclick = function () {
+    if (i === image_options.length - 1) {
+        i = 0;
+    }
+    i = i + 1;
+
+    currentOptionImage.style.backgroundImage = "url(" + image_options[i] + ")";
+
+    // setTimeout(() => {
+    //     currentOptionImage.style.backgroundImage = "url(" + image_options[i] + ")";
+    // }, 50);
+
+};
+
+optionPrevious.onclick = function () {
+    if (i === 0) {
+        i = image_options.length;
+    }
+    i = i - 1;
+
+    setTimeout(() => {
+        currentOptionImage.style.backgroundImage = "url(" + image_options[i] + ")";
+    }, 150);
+
+
+};
+
+
+// -----------------скролл--------------------------
+
+let elementScroll = document.querySelector('.scroll');
+elementScroll.addEventListener('click', scrollBy(0, 1100, "smooth"));
+
+// ------------менюшка бургер--------------
+
